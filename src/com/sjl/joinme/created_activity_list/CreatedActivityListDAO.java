449b4fe672c1590c32b87231545910d84ce264ca
sudhanshugour <sudhanshugour@users.noreply.github.com>
2018-10-28 13:05:43 +0530

    Revert "Merge branch 'master' of https://github.com/joinmestudio/joinme_18_10"
    This reverts commit 8e610f0b280b570e3d4667bd0343afd872aaf0fa, reversing
    changes made to 226fdbe78ff44936e2b653cf2f2cab511d3d283c.

--- src/com/sjl/joinme/created_activity_list/CreatedActivityListDAO.java
+++ src/com/sjl/joinme/created_activity_list/CreatedActivityListDAO.java
@@ -4,13 +4,72 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
+
 import com.sjl.joinme.database.JoinMeDB;
+
+
+
+
+
+
+
+
+
+
+
+
 
 public class CreatedActivityListDAO {
 	private Connection conn = null;
 	private PreparedStatement ps = null;
 	private ResultSet rs = null;
+
+	public boolean addCreatedActivityList(CreatedActivityListDTO dto) {
+		boolean flag = false;
+		try {
+			if (conn == null) {
+				conn = JoinMeDB.getConnection();
+			}
+			String query = "insert into created_activity_list(tag_id, user_id,"
+					+ " cost, activity_name, activity_description, activity_date, status,lat,lng)" + " values(?,?,?,?,?,?,?,?,?)";
+			ps = conn.prepareStatement(query);
+			ps.setInt(1, dto.getTag_id());
+			ps.setInt(2, dto.getUser_id());
+			ps.setInt(3, dto.getCost());
+			ps.setString(4, dto.getActivity_name());
+			ps.setString(5, dto.getActivity_description());
+			ps.setString(6, dto.getActivity_date());
+			ps.setString(7, "" + dto.getStatus());
+			ps.setString(8,dto.getLat());
+			ps.setString(9,dto.getLng());
+			if (ps.executeUpdate() > 0) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			System.out.println("+++Exception in addCreatedActivityList:" + e);
+		} finally {
+			ps = null;
+			conn = null;
+			return flag;
+		}
+	}
+
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	public boolean updateCreatedActivityList(CreatedActivityListDTO dto) {
 		boolean flag = false;
@@ -60,7 +119,6 @@ public class CreatedActivityListDAO {
 		}
 	}
 
-<<<<<<< HEAD
 	
 	
 	
@@ -149,15 +207,11 @@ public class CreatedActivityListDAO {
 
 	public CreatedActivityListDTO getCreatedActivityList(int activity_id) {
 		CreatedActivityListDTO dto = null;
-=======
-	public CreatedActivityListDTO showActivity(int activity_id) {
-		CreatedActivityListDTO dto=null;
->>>>>>> d30d1f5dc51321df88760bfb49648b1903af67a7
 		try {
-			if (conn==null) {
-				conn=JoinMeDB.getConnection();
+			if (conn == null) {
+				conn = JoinMeDB.getConnection();
 			}
-			String query="select * from created_activity_list where activity_id="+activity_id;
+			String query = "select * from created_activity_list where activity_id=" + activity_id;
 			ps = conn.prepareStatement(query);
 			rs = ps.executeQuery();
 			if (rs.next()) {
@@ -176,8 +230,11 @@ public class CreatedActivityListDAO {
 				dto.setCount(rs.getInt("count"));
 			}
 		} catch (Exception e) {
-			System.out.println("+++Exception in showActivity:" + e);
-		}finally {
+			System.out.println("+++Exception in getCreatedActivityList:" + e);
+		} finally {
+			ps = null;
+			rs = null;
+			conn = null;
 			return dto;
 		}
 	}
@@ -217,7 +274,6 @@ public class CreatedActivityListDAO {
 			return al;
 		}
 	}
-<<<<<<< HEAD
 
 
 
@@ -267,63 +323,6 @@ public ArrayList<CreatedActivityListDTO> getAllCreatedActivityListWithTagID(int 
 		return al;
 	}
 }
-=======
-	
-	public ArrayList<CreatedActivityListDTO> getMyActivities(int user_id) {
-		ArrayList<CreatedActivityListDTO> al = new ArrayList<>();
-		CreatedActivityListDTO dto = null;
-		try {
-			if (conn == null) {
-				conn = JoinMeDB.getConnection();
-			}
-			String qurey = "select * from created_activity_list where user_id=" + user_id;
-			ps = conn.prepareStatement(qurey);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				dto = new CreatedActivityListDTO();
-				dto.setActivity_date(rs.getString("activity_date"));
-				dto.setActivity_id(rs.getInt("activity_id"));
-				dto.setActivity_name(rs.getString("activity_name"));
-				dto.setCost(rs.getInt("cost"));
-				dto.setCreated_datetime(rs.getString("created_datetime"));
-				dto.setDescription(rs.getString("description"));
-				dto.setStatus(rs.getString("status").charAt(0));
-				dto.setTag_id(rs.getInt("tag_id"));
-				dto.setUser_id(rs.getInt("user_id"));
-				al.add(dto);
-			}
-		} catch (Exception e) {
-			System.out.println("+++Exception in getMyActivities: " + e);
-		} finally {
-			if (al.isEmpty()) {
-				al=null;
-			}
-			rs=null;
-			ps=null;
-			conn=null;
-			return al;
-		}
-	}
-	
-	public LinkedHashMap<Integer, String> searchActivityByTag(int tag_id) {
-		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
-		try {
-			if (conn == null) {
-				conn = JoinMeDB.getConnection();
-			}
-			String query = "select activity_id, activity_name from created_activity_list where tag_id=" + tag_id;
-			ps = conn.prepareStatement(query);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				map.put(rs.getInt("activityid"), rs.getString("activity_name"));
-			}
-		} catch (Exception e) {
-			System.out.println("+++Exception in searchActivityByTag:" + e);
-		} finally {
-			return map;
-		}
-	}
->>>>>>> d30d1f5dc51321df88760bfb49648b1903af67a7
 }
 
 
